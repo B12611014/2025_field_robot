9d5ee321c4aeaf16823589e52bb8371d0edbd366
B12611014 <b12611014@ntu.edu.tw>
2025-08-19 17:20:53 +0800

    Change the path in train.py and dataset/dtat.yaml

--- train.py
+++ train.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
 # 加載模型
-model = YOLO('yolov8n.pt')  # 選擇您需要的 YOLOv8 模型
+model = YOLO('/content/2025_field_robot/yolov8n.pt')  # 選擇您需要的 YOLOv8 模型
 
 # 訓練模型
-#model.train(data='/Users/outingan/dataset_new/data.yaml', epochs=100, imgsz=640, patience=20,device="mps")
-model.train(data='/Users/outingan/dataset_new_chick_2/data_chick_2.yaml', epochs=200, imgsz=640,patience=50,augment=True,)
-#model.train(data='/Users/outingan/dataset_new_bird/data_bird.yaml', epochs=150, imgsz=640)
+
+model.train(data='/content/2025_field_robot/dataset/data.yaml', epochs=200, imgsz=640,patience=50,augment=True,)
+
 
 
 
